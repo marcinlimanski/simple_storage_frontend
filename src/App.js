d780e6c900679fc09e4e6b6ecb14e5441215418a
Marcin Limanski <marcinlimanski@outlook.com>
2023-08-29 09:35:44 -0400

    src: adding bootstrap layout

--- src/App.js
+++ src/App.js
@@ -4,12 +4,30 @@ import './App.css';
 
 function App() {
   return (
-      <div className="App">
-        <header className="App-header">
-            <GetBalanceForm />
-            <SetBalanceForm />
-        </header>
-      </div>
+      <html lang="en" data-bs-theme="">
+        <body>
+            <main>
+                <div className="container py-4">
+                    <header className="pb-3 mb-4 border-bottom">
+                        <h1>Smart Contract</h1>
+                    </header>
+                    <div className="row align-items-md-stretch">
+                        <div className="col-md-6">
+                            <div className="h-100 p-5 bg-body-tertiary border rounded-3">
+                                <GetBalanceForm />
+                            </div>
+                        </div>
+                        <div className="col-md-6">
+                            <div className="h-100 p-5 bg-body-tertiary border rounded-3">
+                                <SetBalanceForm />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </main>
+        </body>
+      </html>
+
   );
 }
 
